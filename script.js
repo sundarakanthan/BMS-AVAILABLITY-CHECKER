90395a28791b66780431979aad732ef45a247bb2
sundarakanthan <sundarakanthan88@gmail.com>
2024-09-02 13:21:28 +0530

    browser launching version files uploded

---
+++ script.js
@@ -0,0 +1,62 @@
+const twilio = require('twilio');
+const playwright = require('playwright')
+const accountSid = 'AC60a13e697fe18f983c03a73c016b7a28';
+const authToken = '63f12fe4ecb5458427770f2743e85514';
+const client = require('twilio')(accountSid, authToken);
+
+const { chromium } = require('playwright');
+
+
+
+
+async function checkAvailablity() {
+    const browser = await chromium.launch({ headless: false });
+    const page = await browser.newPage();
+
+
+    for (let attempt = 0; attempt < 3; attempt++) {
+        try {
+            await page.goto('https://in.bookmyshow.com/buytickets/the-greatest-of-all-time-chennai/movie-chen-ET00401439-MT/20240905', { waitUntil: 'networkidle' });
+            console.log("Page loaded successfully");
+            break;  // Exit the loop if successful
+        } catch (error) {
+            console.error(`Attempt ${attempt + 1} failed:`, error);
+            if (attempt === 2) {
+                throw error;  
+            }
+        }
+    }
+
+    const theatreName = await page.evaluate(() => document.querySelector('a.showtime-pill[data-venue-code="CBMC"][data-showtime-filter-index="morning"]'));
+    
+    if(theatreName){
+        sendWhatsappMessage1();
+        await browser.close();
+    }else{
+        sendWhatsappMessage2();
+        await browser.close();
+    }
+    
+};
+
+
+ function sendWhatsappMessage1(){
+client.messages
+    .create({
+        body: 'VR mall la ticket irku da bnda..sekrom paru..',
+        from: 'whatsapp:+14155238886',
+        to: 'whatsapp:+919884163253'
+    })
+    .then(message => console.log(message.sid))
+}
+function sendWhatsappMessage2(){
+    client.messages
+        .create({
+            body: 'VR mall la ticket ilaaa..',
+            from: 'whatsapp:+14155238886',
+            to: 'whatsapp:+919884163253'
+        })
+        .then(message => console.log(message.sid))
+    }
+
+setInterval(checkAvailablity,6000)
